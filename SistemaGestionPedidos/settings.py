8f1cfa28c475a3c349a7cde65dc66d5fa155a707
Josue Lopez <tshadez@hotmail.com>
2020-08-27 21:42:59 -0500

    gitignore updatee

--- SistemaGestionPedidos/settings.py
+++ SistemaGestionPedidos/settings.py
@@ -79,14 +79,13 @@ WSGI_APPLICATION = 'SistemaGestionPedidos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 # 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 """
 DATABASES = {
     'default': {
@@ -98,7 +97,6 @@ DATABASES = {
         'PORT': 3306,
     }
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
