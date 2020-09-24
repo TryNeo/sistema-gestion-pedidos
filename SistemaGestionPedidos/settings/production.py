95c0e415d86f65b10d7801a0b68786a5a74abe06
josu3 <tshadez@hotmail.com>
2020-09-24 15:36:36 -0500

    .

--- SistemaGestionPedidos/settings/production.py
+++ SistemaGestionPedidos/settings/production.py
@@ -5,7 +5,7 @@ from SistemaGestionPedidos.settings.base import *
 # 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['asoprotosue-pedidos.herokuapp.com']
 
