0d2dda4a137940783e55d5b6aba9be97c00329e0
josu3 <tshadez@hotmail.com>
2020-09-24 15:06:11 -0500

    .

--- SistemaGestionPedidos/settings/base.py
+++ SistemaGestionPedidos/settings/base.py
@@ -21,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = 'c6^*d8qlhmua7*!!x&t=cf_n6llo-!+5@@w8-+eu0q5ka54ke9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
