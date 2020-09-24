4364f39081ffe9e10c95813c0dc1132774e0f347
josu3 <tshadez@hotmail.com>
2020-09-23 19:20:23 -0500

    Update 23/09/2020

--- SistemaGestionPedidos/settings.py
+++ SistemaGestionPedidos/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c6^*d8qlhmua7*!!x&t=cf_n6llo-!+5@@w8-+eu0q5ka54ke9'
+
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
